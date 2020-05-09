0a69f5043fc8f33ad44ef04a8c972818bd96108e
Jan Staudenmaier <staudenmaier@fias.uni-frankfurt.de>
2020-05-09 23:51:40 +0200

    Implement rudimentary countdown timer

--- BrewTime/ViewController.swift
+++ BrewTime/ViewController.swift
@@ -13,17 +13,33 @@ class ViewController: UIViewController {
     @IBOutlet var startTimerButtron: UIButton!
     @IBOutlet var timeLabel: UILabel!
     
+    var timer = Timer()
+    var timeLeft = 10
+    
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        timeLeft -= 1
+        timeLabel.text = "\(timeLeft)"
+        if timeLeft <= 0 {
+            timer.invalidate()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
         timeLabel.isHidden = true
     }
 
     @IBAction func startPressed(_ sender: Any) {
         print("Start timer button was pressed")
+        timeLabel.text = "\(timeLeft)"
         timeLabel.isHidden = false
+        startTimerButtron.isHidden = true
+        runTimer()
     }
     
 }
--- TODO.md
+++ TODO.md
@@ -1,8 +1,19 @@
 # TODO
 
+## Next
+
 - [x] Settle on idea (_coffee timer and recipes_)
 - [x] Find project name (_brew time_)
 - [x] Rename repos according to project name
 - [x] Watch WWDC intro video on how to code an app (Started watching, created vanilla xcode project)
 - [ ] Learn some swift
-- [ ] Implement proper timer in prototype
+- [x] Implement first timer in prototype
+- [ ] Find out what is necessary to test on device (Apply for developer account?)
+- [ ] Think about what I need for Timer UI (e.g. total time, current step, next step, ...)
+- [ ] Create timer UI elements on storyboard
+
+
+## Backlog
+
+- [ ] Change name: already an app named Brewtime
+- [ ] Add license to make public/open source
